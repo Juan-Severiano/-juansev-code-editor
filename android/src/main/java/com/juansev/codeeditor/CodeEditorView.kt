915ff4ee55fdb31e50462086c07704cd4247c9b0
Francisco Juan <juansoussev@gmail.com>
2024-10-09 20:36:38 -0300

    initial commit

--- android/src/main/java/com/juansev/codeeditor/CodeEditorView.kt
+++
@@ -1,7 +0,0 @@
-package com.juansev.codeeditor
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class CodeEditorView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
